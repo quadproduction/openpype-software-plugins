f0f5eccec035eb860f903dd008cb69b09cbae102
BenSouchet <contact@bensouchet.dev>
2024-01-24 18:03:38 +0100

    Re-enable Studio Library installation

--- host/maya/python/studiolibrary_installer.py
+++ host/maya/python/studiolibrary_installer.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+plugin_name = "Studio Library"
 plugin_version = "2.13.2"
 plugin_package_path = Path("/prod/softprod/apps/studiolibrary/{}/linux".format(plugin_version))
 plugin_install_script_name = "install.py"
@@ -9,4 +10,17 @@ def install():
     globals_dict = globals()
     globals_dict["__file__"] = plugin_package_script_path.resolve()
 
-    #exec(open(plugin_package_script_path).read(), globals_dict)
+    # Check if the Studio Library shelf icon is already present on the shelf of the user
+    # If yes delete all occurrences
+    import maya.cmds as cmds
+
+    shelf_name = "Polygons"
+    shelf_elems = cmds.shelfLayout(shelf_name, query=True, fullPathName=True, childArray=True)
+
+    for shelf_elem in shelf_elems:
+        shelf_elem_fullname = "shelf_name|{}".format(shelf_name, shelf_elem)
+        if cmds.objectTypeUI(shelf_elem, isType="shelfButton") and \
+            cmds.shelfButton(shelf_elem_fullname, query=True, annotation=True) == plugin_name:
+            cmds.deleteUI(shelf_elem_fullname, control=True)
+
+    exec(open(plugin_package_script_path).read(), globals_dict)
